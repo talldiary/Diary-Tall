590f7e4d89131700ae2b5fa20056c02626068543
talldiary <talldiary37@gmail.com>
2026-02-18 14:42:49 +0000

    Add files via upload

---
+++ cv.py
@@ -0,0 +1,50 @@
+import streamlit as st
+st.title("mon porforiot")
+sidebar=st.sidebar
+with sidebar:
+    st.image("pp.jfif")
+    st.write("Diary")
+    st.write("nom:Tall")
+    st.write("adresse:Noflaye")
+    st.write("telephone:768875141")
+    st.write("contact:talldiary37@gmail.com")
+
+###########################################
+
+st.header("a propos")
+st.write("Geomaticienne et geographe titulaire d'une licence,maitrisant les outils SIG(QGIS,ARCGIS) et la cartographie.capable d'analyser et de valoriser des donnees spatiales pour l'aide a la decision.")
+
+##########################################
+
+st.header("Projet")
+st.markdown("""analyse spatiale de l'occupation du sol a l'aide des SIG
+objectif:collecte et traitement de donnees geographiques,production de cartes thematiques et analyse spatiale
+outils:QGIS/ARCGIS
+resultats: capable d'aider a la prise de decison pour l'occupation du sol
+""")
+############################################
+
+st.header("DIPLOME")
+st.write("technicienne superieur en geomatique")
+st.write("licence geographie")
+st.write("Baccalaureat")
+
+#############################################
+
+st.header("COMPETENCES")
+st.write("creation de base de donnees geographiques dans QGIS et ARCGIS")
+st.write("georeferencement et digitalisation dans QGIS et ARCGIS")
+st.write("production de carte")
+st.write("collecte et traitement de donnees")
+st.write("bureautique(word,excel,powerpoint")
+
+##################################################
+
+st.header("EXPERIENCE")
+
+###################################################
+
+st.header("CENTRE D'INTERET")
+st.write("footing")
+st.write("footbal")
+st.write("art materiaux")
